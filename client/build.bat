0384ed6867dd6c6afc664469528d4a1cd885456e
Philpax <me@philpax.me>
2022-05-22 10:09:26 +0200

    chore: switch to workspaces

--- client/build.bat
+++
@@ -1,1 +0,0 @@
-cargo build --release && wasm-bindgen --out-name sdfbox-client --out-dir target --target web target/wasm32-unknown-unknown/release/sdfbox-client.wasm
--- client/build.sh
+++ client/build.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-cargo build --release && wasm-bindgen --out-name sdfbox-client --out-dir target --target web target/wasm32-unknown-unknown/release/sdfbox-client.wasm
+#!/bin/bash
+set -o errexit
+
+mkdir -p build
+rm -rf build/*
+cargo build --release
+wasm-bindgen --out-name sdfbox-client --out-dir build --target web ../target/wasm32-unknown-unknown/release/sdfbox-client.wasm
+cp assets/* build
+cp -r build ../server/assets
