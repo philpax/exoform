d8fcc501fce14350cbe37168109f4a85fa2b1942
Philpax <me@philpax.me>
2022-05-31 01:50:29 +0200

    feat: implement kdl-based generation

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,5 @@
 use bevy::{input::mouse::MouseMotion, prelude::*};
-use bevy_egui::{EguiContext, EguiPlugin};
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 use smooth_bevy_cameras::{
     controllers::orbit::{
@@ -8,13 +8,39 @@ use smooth_bevy_cameras::{
     LookTransformPlugin,
 };
 
+use shared::{code_to_node, Node};
+
+#[derive(Debug, PartialEq)]
+pub struct GraphDescription(String);
+
+#[derive(Debug)]
+pub struct ParsedGraph(anyhow::Result<Node>);
+
+pub struct CurrentEntity(Option<Entity>);
+
+pub struct RebuildTimer(Timer);
+
 pub fn main() {
     #[cfg(target_arch = "wasm32")]
     console_error_panic_hook::set_once();
 
+    let description_base = r#"
+union {
+    sphere x=0 y=0 z=0 r=1
+    sphere x=1 y=0 z=0 r=1
+}
+"#;
+
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .insert_resource(bevy::winit::WinitSettings::desktop_app())
+        .insert_resource(GraphDescription(description_base.trim().to_string()))
+        .insert_resource(ParsedGraph(Err(anyhow::anyhow!("unparsed graph"))))
+        .insert_resource(CurrentEntity(None))
+        .insert_resource(RebuildTimer(Timer::new(
+            std::time::Duration::from_secs(1),
+            true,
+        )))
         .add_plugins(DefaultPlugins)
         .add_plugin(LookTransformPlugin)
         .add_plugin(OrbitCameraPlugin::new(true))
@@ -22,20 +48,60 @@ pub fn main() {
         .add_plugin(EguiPlugin)
         .add_startup_system(setup)
         .add_system(input_map)
+        .add_system(parse_code)
+        .add_system(sdf_code_editor)
+        .add_system(keep_rebuilding_mesh)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+fn node_to_saft_node(graph: &mut saft::Graph, node: &Node) -> saft::NodeId {
+    match node {
+        Node::Sphere { position, radius } => graph.sphere(*position, *radius),
+        Node::Union(size, node1, node2) => {
+            let lhs = node_to_saft_node(graph, &node1);
+            let rhs = node_to_saft_node(graph, &node2);
+            if *size == 0.0 {
+                graph.op_union(lhs, rhs)
+            } else {
+                graph.op_union_smooth(lhs, rhs, *size)
+            }
+        }
+    }
+}
+
+fn node_to_saft(root: &Node) -> (saft::Graph, saft::NodeId) {
+    let mut graph = saft::Graph::default();
+    let root_id = node_to_saft_node(&mut graph, root);
+    (graph, root_id)
+}
+
+fn create_mesh(
+    commands: &mut Commands,
+    meshes: &mut Assets<Mesh>,
+    materials: &mut Assets<StandardMaterial>,
+    current_entity: &mut CurrentEntity,
+    root: &Node,
 ) {
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    let (graph, root) = node_to_saft(root);
+    let mesh = sdf_to_bevy_mesh(graph, root);
+
+    if let Some(entity) = current_entity.0 {
+        commands.entity(entity).despawn();
+    }
+
+    current_entity.0 = Some(
+        commands
+            .spawn_bundle(PbrBundle {
+                mesh: meshes.add(mesh),
+                material: materials.add(Color::WHITE.into()),
+                transform: Transform::from_xyz(0.0, 0.5, 0.0),
+                ..default()
+            })
+            .id(),
+    );
+}
+
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
@@ -51,6 +117,32 @@ fn setup(
         Vec3::new(-2.0, 5.0, 5.0),
         Vec3::new(0., 0., 0.),
     ));
+}
+
+fn sdf_to_bevy_mesh(graph: saft::Graph, root: saft::NodeId) -> Mesh {
+    use bevy::render::mesh as brm;
+    let triangle_mesh = saft::mesh_from_sdf(&graph, root, saft::MeshOptions::default()).unwrap();
+    let mut mesh = Mesh::new(brm::PrimitiveTopology::TriangleList);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, triangle_mesh.normals);
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_UV_0,
+        triangle_mesh
+            .positions
+            .iter()
+            .map(|_| [0.0, 0.0])
+            .collect::<Vec<_>>(),
+    );
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, triangle_mesh.positions);
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_COLOR,
+        triangle_mesh
+            .colors
+            .into_iter()
+            .map(|[r, g, b]| Color::rgb(r, g, b).as_rgba_u32())
+            .collect::<Vec<_>>(),
+    );
+    mesh.set_indices(Some(brm::Indices::U32(triangle_mesh.indices)));
+    mesh
 }
 
 pub fn input_map(
@@ -89,6 +181,72 @@ pub fn input_map(
             ));
         } else {
             events.send(ControlEvent::Orbit(mouse_rotate_sensitivity * cursor_delta));
+        }
+    }
+}
+
+fn parse_code(mut parsed_graph: ResMut<ParsedGraph>, graph_description: Res<GraphDescription>) {
+    *parsed_graph = ParsedGraph(code_to_node(&graph_description.0));
+}
+
+fn sdf_code_editor(
+    mut egui_context: ResMut<EguiContext>,
+    mut graph_description: ResMut<GraphDescription>,
+    parsed_graph: Res<ParsedGraph>,
+) {
+    let ctx = egui_context.ctx_mut();
+
+    egui::SidePanel::left("left_panel")
+        .default_width(300.0)
+        .show(ctx, |ui| {
+            egui::TopBottomPanel::bottom("bottom_panel")
+                .resizable(false)
+                .show_inside(ui, |ui| {
+                    if let Err(err) = &parsed_graph.0 {
+                        ui.label(err.to_string());
+                    }
+                });
+
+            egui::CentralPanel::default().show_inside(ui, |ui| {
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    ui.add_sized(
+                        ui.available_size(),
+                        egui::TextEdit::multiline(&mut graph_description.0)
+                            .font(egui::TextStyle::Monospace) // for cursor height
+                            .code_editor()
+                            .lock_focus(true)
+                            .desired_width(f32::INFINITY),
+                    );
+                });
+            });
+        });
+
+    egui::SidePanel::right("right_panel")
+        .default_width(300.0)
+        .show(ctx, |ui| {
+            ui.label("This is where the list of users would go");
+        });
+}
+
+fn keep_rebuilding_mesh(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut current_entity: ResMut<CurrentEntity>,
+    mut rebuild_timer: ResMut<RebuildTimer>,
+    parsed_graph: Res<ParsedGraph>,
+    time: Res<Time>,
+) {
+    rebuild_timer.0.tick(time.delta());
+    if let Ok(root) = &parsed_graph.0 {
+        if rebuild_timer.0.finished() {
+            create_mesh(
+                &mut commands,
+                &mut meshes,
+                &mut materials,
+                &mut current_entity,
+                root,
+            );
         }
     }
 }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,109 @@
+use anyhow::Context;
+use bevy_math::prelude::*;
 
+#[derive(Debug, PartialEq)]
+pub enum Node {
+    Sphere { position: Vec3, radius: f32 },
+    Union(f32, Box<Node>, Box<Node>),
+}
+
+fn get_f32_attr(node: &kdl::KdlNode, key: &str) -> anyhow::Result<f32> {
+    let value = node.get(key).context(format!("expected {}", key))?.value();
+
+    value
+        .as_i64()
+        .map(|v| v as f32)
+        .or(value.as_f64().map(|v| v as f32))
+        .context("expected float")
+}
+
+fn parse_node(node: &kdl::KdlNode) -> anyhow::Result<Node> {
+    match node.name().value() {
+        "sphere" => {
+            let x = get_f32_attr(node, "x")?;
+            let y = get_f32_attr(node, "y")?;
+            let z = get_f32_attr(node, "z")?;
+            let r = get_f32_attr(node, "r")?;
+
+            Ok(Node::Sphere {
+                position: Vec3::new(x, y, z),
+                radius: r,
+            })
+        }
+        "union" => {
+            let children = node
+                .children()
+                .context("expected children for union")?
+                .nodes();
+            if children.len() != 2 {
+                anyhow::bail!("expected two children for union");
+            }
+
+            let size = if node.get("size").is_some() {
+                get_f32_attr(node, "size")?
+            } else {
+                0.0
+            };
+
+            Ok(Node::Union(
+                size,
+                Box::new(parse_node(&children[0])?),
+                Box::new(parse_node(&children[1])?),
+            ))
+        }
+        _ => anyhow::bail!("unsupported node type"),
+    }
+}
+
+pub fn code_to_node(description: &str) -> anyhow::Result<Node> {
+    let doc: kdl::KdlDocument = description.parse()?;
+
+    if doc.nodes().len() != 1 {
+        anyhow::bail!("expected only one root node")
+    }
+
+    parse_node(&doc.nodes()[0])
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn can_parse_basic_union() {
+        let input = r#"
+union {
+    sphere x=0 y=0 z=0 r=1
+    sphere x=1 y=0 z=0 r=2.5
+}
+"#;
+
+        assert_eq!(
+            code_to_node(input).ok(),
+            Some(Node::Union(
+                0.0,
+                Box::new(Node::Sphere {
+                    position: Vec3::new(0.0, 0.0, 0.0),
+                    radius: 1.0
+                }),
+                Box::new(Node::Sphere {
+                    position: Vec3::new(1.0, 0.0, 0.0),
+                    radius: 2.5
+                })
+            ))
+        );
+    }
+
+    #[test]
+    fn fails_on_multiple_root_nodes() {
+        let input = r#"
+sphere x=0 y=0 z=0 r=1
+sphere x=1 y=0 z=0 r=2.5
+"#;
+
+        assert_eq!(
+            code_to_node(input).err().unwrap().to_string(),
+            "expected only one root node"
+        );
+    }
+}
