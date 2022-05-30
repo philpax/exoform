e50e337a17fbe35f9b034037d230a7af4ed85f2c
Philpax <me@philpax.me>
2022-05-30 18:56:55 +0200

    fix(client): make build script less broken

--- client/build.sh
+++ client/build.sh
@@ -3,7 +3,8 @@ set -o errexit
 
 mkdir -p build
 rm -rf build/*
-cargo build --release
+cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --out-name sdfbox-client --out-dir build --target web ../target/wasm32-unknown-unknown/release/sdfbox-client.wasm
 cp assets/* build
-cp -r build ../server/assets
+rm -r ../server/assets/*
+cp -r build/* ../server/assets/
